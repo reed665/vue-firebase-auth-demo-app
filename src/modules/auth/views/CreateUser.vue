497e0479f3598157cf82fc0ad2ef30cc0505479f
reed665 <reed665@hotmail.com>
2020-03-08 10:07:37 +0300

    CreateUser basic template markup

--- src/modules/auth/views/CreateUser.vue
+++ src/modules/auth/views/CreateUser.vue
@@ -1,9 +1,66 @@
 <template>
+  <!-- eslint-disable vue/multiline-html-element-content-newline -->
+  <!-- eslint-disable vue/singleline-html-element-content-newline -->
   <div class="create-user">
-    <h2>This is Create User view</h2>
+    <h2>Create User</h2>
 
-    <router-link :to="{ name: 'signin' }">
-      Sign In
-    </router-link>
+    <form @submit.prevent="submit">
+      <label class="form-item">
+        <span class="label-text">Email</span>
+
+        <input
+          required
+          type="email"
+        >
+      </label>
+
+      <label class="form-item">
+        <span class="label-text">Password</span>
+
+        <input
+          required
+          type="password"
+        >
+      </label>
+
+      <div class="form-item">
+        <button>Create user</button>
+      </div>
+    </form>
+
+    <p>
+      Or <router-link :to="{ name: 'signin' }">Sign In</router-link> instead
+    </p>
   </div>
 </template>
+
+<script>
+// import {} from '@vue/composition-api';
+
+export default {
+  setup() {
+    const submit = (event) => {
+      console.log(event);
+    };
+
+    return {
+      submit,
+    };
+  },
+
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+}
+
+.form-item {
+  margin: 5px 0;
+  display: flex;
+  flex-direction: column;
+}
+</style>
